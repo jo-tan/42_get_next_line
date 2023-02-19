5bf86d1c4913ab1e81a726d7a084e8dee2c5391c
Jo-hsi TAN <jo-tan@e2r2p6.clusters.42paris.fr>
2023-02-19 08:08:41 +0100

    update mandatory and bonus files which pass the tester. 19 Feb 2023

---
+++ practice/get_next_line.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jo-tan <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/28 16:58:17 by jo-tan            #+#    #+#             */
+/*   Updated: 2023/02/19 04:35:59 by jo-tan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <stdio.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlen(const char *s);
+char	*ft_strjoin(char *s1, char *s2);
+
+#endif
---
+++ practice/get_next_line_utils.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jo-tan <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/28 17:56:27 by jo-tan            #+#    #+#             */
+/*   Updated: 2023/02/15 06:23:23 by jo-tan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	int	len;
+
+	len = 0;
+	while (s[len])
+		len++;
+	return (len);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int		i;
+	char	ch;
+
+	ch = (unsigned char)c;
+	i = 0;
+	if (!s)
+		return (NULL);
+	while (s[i] != '\0')
+	{
+		if (s[i] == ch)
+			return ((char *)(&s[i]));
+		i++;
+	}
+	if (ch == '\0')
+		return ((char *)&s[i]);
+	return (NULL);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char			*res;
+	size_t			i;
+	size_t			l;
+
+	i = 0;
+	l = ft_strlen(s1) + ft_strlen(s2);
+	res = (char *)malloc((l + 1) * sizeof(char));
+	if (!res)
+		return (NULL);
+	while (s1[i])
+	{
+		res[i] = s1[i];
+		i++;
+	}
+	l = 0;
+	while (s2[l])
+	{
+		res[i + l] = s2[l];
+		l++;
+	}
+	res[i + l] = '\0';
+	free(s1);
+	return (res);
+}
